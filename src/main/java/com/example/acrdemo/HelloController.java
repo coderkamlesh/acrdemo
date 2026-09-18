927bfe68136c49fa04f867f039f22c2e32a747ab
kamlesh <kamlesh@easyswift.in>
2026-09-18 18:47:30 +0530

    added json reposnse in hello endpoint

--- src/main/java/com/example/acrdemo/HelloController.java
+++ src/main/java/com/example/acrdemo/HelloController.java
@@ -4,11 +4,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     ResponseEntity<?> hello(){
-        return ResponseEntity.ok("hello from testing");
+        return ResponseEntity.ok(Map.of("message", "hello from testing"));
     }
 }
