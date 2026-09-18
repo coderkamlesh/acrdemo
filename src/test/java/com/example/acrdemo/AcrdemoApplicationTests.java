68cb538abd51bdd98a0fe4779139731c37d490c4
kamlesh <kamlesh@easyswift.in>
2026-09-18 14:03:15 +0530

    initial commit

---
+++ src/test/java/com/example/acrdemo/AcrdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acrdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcrdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
