30ec7180c20cab04a636f78677b58aaa5a5283b3
kamlesh kumar <coderkamlesg68@gmail.com>
2026-09-20 20:44:13 +0530

    automatic deploye aws lambda

--- src/main/java/com/example/acrdemo/HealthController.java
+++ src/main/java/com/example/acrdemo/HealthController.java
@@ -1,12 +1,21 @@
 package com.example.acrdemo;
 
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HealthController {
     @GetMapping("/health")
     public String health() {
         return "ok";
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> index() {
+        return ResponseEntity.ok(Map.of("status",true,"message","running"));
     }
 }
